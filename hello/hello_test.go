b0c6857903402aceb0a3dea9631edd919fe81c30
gkallingal <george.kallingal@sony.com>
2020-11-30 14:54:21 -0800

    added new test to check when empty string is passed as a parameter

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,6 +3,15 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
+	request := SayHello("")
+	response := "Hello, World"
+
+	if request != response {
+		t.Errorf("There was an error.  Expected to get %q but received %q\n", response, request)
+	}
+}
+
+func TestHelloWithArgument(t *testing.T) {
 	request := SayHello("George")
 	response := "Hello, George"
 

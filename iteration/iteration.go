fbde339ff41c35189027ce16417b69bcb7cb9f81
gkallingal <george.kallingal@sony.com>
2020-12-02 13:26:15 -0800

    update Repeat function to use strings.Repeat function

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,15 +1,13 @@
 package iteration
 
+import "strings"
+
 const (
 	repeatCount = 5
 )
 
 //Repeat function will take any character and repeat it N number of times based on count parameter
 func Repeat(letter string, count int) string {
-	var repeat string
-
-	for i := 0; i < count; i++ {
-		repeat += letter
-	}
+	repeat := strings.Repeat(letter, count)
 	return repeat
 }

da6c04254505b3b2830f1d2c91c43ee63ae2d325
gkallingal <george.kallingal@sony.com>
2020-12-02 13:00:55 -0800

    add documentation for Repeat function

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,10 +1,15 @@
 package iteration
 
+const (
+	repeatCount = 5
+)
+
+//Repeat function will take any character and repeat it 5 times
 func Repeat(letter string) string {
 	var repeat string
 
-	for i := 0; i < 5; i++ {
-		repeat = repeat + letter
+	for i := 0; i < repeatCount; i++ {
+		repeat += letter
 	}
 	return repeat
 }

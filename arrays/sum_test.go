4e12cace4d07d478106daa3bec9b74d457a1055c
gkallingal <george.kallingal@sony.com>
2020-12-04 10:45:11 -0800

    add test to sum a slice

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,14 +3,29 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	//declare array
-	numbers := [5]int{1, 1, 1, 1, 2}
 
-	got := Sum(numbers)
+	t.Run("calculate sum of an array", func(t *testing.T) {
+		//declare array
+		numbers := [5]int{1, 1, 1, 1, 2}
 
-	want := 6
+		got := Sum(numbers)
 
-	if got != want {
-		t.Errorf("Expected: %d, Got: %d, Given: %v\n", want, got, numbers)
-	}
+		want := 6
+
+		if got != want {
+			t.Errorf("Expected: %d, Got: %d, Given: %v\n", want, got, numbers)
+		}
+	})
+
+	t.Run("calculate sum of a slice", func(t *testing.T) {
+		numbers := []int{2, 4, 6}
+
+		got := Sum(numbers)
+
+		want := 12
+
+		if got != want {
+			t.Errorf("Got %d, Wanted %d, Given %v\n", got, want, numbers)
+		}
+	})
 }

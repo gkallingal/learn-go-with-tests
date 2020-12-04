4fe5a7156f17e1c5a5314669d70448ee57f1f23b
gkallingal <george.kallingal@sony.com>
2020-12-04 10:51:47 -0800

    Removing one of the tests due to redundancy

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,19 +4,6 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	t.Run("calculate sum of an array", func(t *testing.T) {
-		//declare array
-		numbers := []int{1, 1, 1, 1, 2}
-
-		got := Sum(numbers)
-
-		want := 6
-
-		if got != want {
-			t.Errorf("Expected: %d, Got: %d, Given: %v\n", want, got, numbers)
-		}
-	})
-
 	t.Run("calculate sum of a slice", func(t *testing.T) {
 		numbers := []int{2, 4, 6}
 

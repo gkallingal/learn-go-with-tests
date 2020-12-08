0a9dec55953731ea2923ea2188ae707f24e6352d
gkallingal <george.kallingal@sony.com>
2020-12-08 11:24:53 -0800

    add balance field to Wallet struct. update deposit and balance methods to leverage balance field

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,12 +1,14 @@
 package pointers
 
 type Wallet struct {
+	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
+	w.balance += amount
 
 }
 
 func (w Wallet) Balance() int {
-	return 0
+	return w.balance
 }

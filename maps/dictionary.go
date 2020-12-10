ad8fd06a6cbd0fdfde91186f4d639d66e8bcaf74
gkallingal <george.kallingal@sony.com>
2020-12-10 05:54:26 -0800

    add Search method for Dictionary type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,3 +5,7 @@ type Dictionary map[string]string
 func Search(dictionary map[string]string, word string) string {
 	return dictionary[word]
 }
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}

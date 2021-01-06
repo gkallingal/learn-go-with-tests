c0bd19c3fbf0582090d662a527cd7dcf5f14db10
gkallingal <george.kallingal@sony.com>
2021-01-06 11:53:38 -0800

    refactor WebsiteRacer func to leverage select against channels; us go routines to run url checks concurrently

--- selects/racer.go
+++ selects/racer.go
@@ -2,21 +2,22 @@ package selects
 
 import (
 	"net/http"
-	"time"
 )
 
 func WebsiteRacer(x, y string) string {
-	durationX := measureResponse(x)
-	durationY := measureResponse(y)
-
-	if durationX < durationY {
+	select {
+	case <-ping(x):
 		return x
+	case <-ping(y):
+		return y
 	}
-	return y
 }
 
-func measureResponse(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
